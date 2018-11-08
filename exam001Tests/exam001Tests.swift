c5d49e30bb6af67704f4b92a574b6b1992716420
Adnan Al-Akhrass <adnan.alakhrass@weightwatchers.com>
2018-11-08 09:50:54 +0200

    Initial Project

---
+++ exam001Tests/exam001Tests.swift
@@ -0,0 +1,34 @@
+//
+//  exam001Tests.swift
+//  exam001Tests
+//
+//  Created by Adnan Al-Akhrass on 11/8/18.
+//  Copyright © 2018 Adnan Al-Akhrass. All rights reserved.
+//
+
+import XCTest
+@testable import exam001
+
+class exam001Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
